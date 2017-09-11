287068e78ac6fab4988c6914e9ee7a2f7e2d4f6b
Jake Sanchez <jacob.sanchez@huskers.unl.edu>
2017-09-10 20:55:26 -0400

    broke out profile into components

---
+++ components/Profile/UserXPTable.js
@@ -0,0 +1,71 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import commaNumber from 'comma-number'
+
+import {
+  formatDate,
+  LONG_DATE_STRING,
+  getLevelForXP,
+} from '../../shared/utils'
+
+export default class UserXPTable extends Component {
+  onXpUpdateClick(event) {
+  	event.preventDefault()
+  }
+
+  renderXpTable(xpUpdates) {
+    if (!xpUpdates.length) {
+      return (<p className="mt-3 text-center">No XP updates entered.</p>)
+    }
+
+    // sort chronologically
+    const updates = xpUpdates.sort((u1, u2) => {
+      return new Date(u2.date) - new Date(u1.date)
+    })
+
+    // map to row
+    var rows = []
+    updates.forEach(u => {
+      rows.push(
+        <tr key={ new Date(u.date).getTime() }>
+          <td>{ commaNumber(u.value) }</td>
+          <td>{ getLevelForXP(u.value) }</td>
+          <td>{ formatDate(u.date, LONG_DATE_STRING) }</td>
+        </tr>
+      )
+    })
+
+    return (
+      <div className="mt-3" style={{ height: "400px", overflow: "auto" }}>
+        <table className="table table-hover">
+          <thead>
+            <tr>
+              <th>XP</th>
+              <th>Level</th>
+              <th>Date</th>
+            </tr>
+          </thead>
+          <tbody>
+            { rows }
+          </tbody>
+        </table>
+      </div>
+    )
+  }
+
+  render() {
+  	const { updates } = this.props 
+    return (
+      <div>
+        { this.renderXpTable(updates) }
+        <div className="row justify-content-end mt-3 mr-1">
+          <a href="#" data-toggle="modal" data-target="#xpUpdateModal" onClick={this.onXpUpdateClick.bind(this)}>Update XP</a>
+        </div>
+      </div>
+    )
+  }
+}
+
+UserXPTable.propTypes = {
+  updates: PropTypes.array.isRequired,
+}
