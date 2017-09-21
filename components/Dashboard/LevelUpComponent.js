9bf9c667ed79664edeaf3ec95375ecf64296bbd8
Jake Sanchez <jacob.sanchez@huskers.unl.edu>
2017-09-20 20:52:22 -0400

    percent to leveling - :shipit: - closes #46

--- components/Dashboard/LevelUpComponent.js
+++ components/Dashboard/LevelUpComponent.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import Moment from 'moment'
 import commaNumber from 'comma-number'
+import toPercentage from 'to-percentage'
 
 import {
   LONG_DATE_STRING,
@@ -14,10 +15,15 @@ const lastN = 7
 
 export default class LevelUpComponent extends Component {
   calcXpTilNextLevel(xp) {
-    const nextLevel = parseInt(getLevelForXP(xp)) + 1
+    const level = parseInt(getLevelForXP(xp))
+    const nextLevel = level + 1
+    const currentLevelXp = minXPForLevel[level]
+    const rawPercent = (xp - currentLevelXp) / (minXPForLevel[nextLevel] - currentLevelXp)
+
     return {
       nextLevel,
       xpTilNextLevel: minXPForLevel[nextLevel] - xp,
+      percentTowardsNextLevel: toPercentage(rawPercent, 1)
     }
   }
 
@@ -30,7 +36,7 @@ export default class LevelUpComponent extends Component {
     const lastNUpdates = updates.slice(updates.length - num)
 
     const dailyAvg = parseInt((lastNUpdates[num - 1].value - lastNUpdates[0].value) / (num - 1))
-    const { nextLevel, xpTilNextLevel } = this.calcXpTilNextLevel(lastNUpdates[num - 1].value)
+    const { nextLevel, xpTilNextLevel, percentTowardsNextLevel } = this.calcXpTilNextLevel(lastNUpdates[num - 1].value)
     const daysTilNextLevel =  Math.ceil(xpTilNextLevel / dailyAvg)
     const projectedLevelUpDate = Moment().add(daysTilNextLevel, 'days').format(LONG_DATE_STRING)
 
@@ -40,6 +46,7 @@ export default class LevelUpComponent extends Component {
       xpTilNextLevel,
       daysTilNextLevel,
       projectedLevelUpDate,
+      percentTowardsNextLevel,
     }
   }
 
@@ -59,13 +66,14 @@ export default class LevelUpComponent extends Component {
           <div className="card-block">
             <div className="row">
               <div className="col">Next Level: { data.nextLevel }</div>
-              <div className="col">XP Needed for Next Level: { commaNumber(data.xpTilNextLevel) }</div>
+              <div className="col">Progress to Level { data.nextLevel}: { data.percentTowardsNextLevel }</div>
             </div>
             <div className="row">
               <div className="col">Daily Average: { commaNumber(data.dailyAvg) }</div>
               <div className="col">Days Until Level Up: { data.daysTilNextLevel }</div>
             </div>
             <div className="row">
+              <div className="col">XP Needed for Next Level: { commaNumber(data.xpTilNextLevel) }</div>
               <div className="col">Projected Level Up on { data.projectedLevelUpDate }</div>
             </div>
           </div>
