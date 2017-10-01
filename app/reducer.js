965cd34024448dfd14e02b0b6ad3ef3188d5cec4
Jake Sanchez <jacob.sanchez@huskers.unl.edu>
2017-10-01 16:52:32 -0400

    clear state on logout

--- app/reducer.js
+++ app/reducer.js
@@ -19,18 +19,19 @@ import {
 const trainerActions = [LOGIN, LOGOUT, SET_STATUS, FETCH_TRAINER, VERIFY_TRAINER, UPDATE_TRAINER]
 const dashboardActions = [FETCH_TRAINERS, CREATE_TRAINER, DELETE_TRAINER, CREATE_CATCH, FETCH_CATCHES, SET_MAP_SCOPE, SET_USER_SCOPE]
 
-function trainerReducer(state = {
+const defaultTrainerState = {
   trainer: JSON.parse(localStorage.getItem('trainer')),
-}, action) {
+}
+
+function trainerReducer(state = defaultTrainerState, action) {
+  if (action.type == LOGOUT)
+    return defaultTrainerState
+
   if (trainerActions.indexOf(action.type) == -1)
     return state
 
   var trainer = action.trainer || state.trainer
   var message = action.message
-
-  if (action.type == LOGOUT) {
-    trainer = null
-  }
 
   return {
     ...state,
@@ -39,12 +40,17 @@ function trainerReducer(state = {
   }
 }
 
-function dashboardReducer(state = {
+const defaultDashboardState = {
   trainers: [],
   catches: null,
   mapScope: MapScopes.USA,
   userScope: UserScopes.ME,
-}, action) {
+}
+
+function dashboardReducer(state = defaultDashboardState, action) {
+  if (action.type == LOGOUT)
+    return defaultDashboardState
+
   if (dashboardActions.indexOf(action.type) == -1)
     return state
 
@@ -54,27 +60,22 @@ function dashboardReducer(state = {
   var userScope = state.userScope
   var message = action.message
 
-  if (action.type == CREATE_TRAINER && action.trainer) {
+  if (action.type == CREATE_TRAINER && action.trainer)
     trainers = trainers.concat(action.trainer)
-  }
 
-  if (action.type == DELETE_TRAINER && action.trainerId) {
+  if (action.type == DELETE_TRAINER && action.trainerId)
     trainers = trainers.filter(tr => {
       return tr._id != action.trainerId
     })
-  }
 
-  if (action.type == CREATE_CATCH && action.catch) {
+  if (action.type == CREATE_CATCH && action.catch)
     catches = catches.concat(action.catch)
-  }
 
-  if (action.type == SET_MAP_SCOPE && action.scope) {
+  if (action.type == SET_MAP_SCOPE && action.scope)
     mapScope = action.scope
-  }
 
-  if (action.type == SET_USER_SCOPE && action.scope) {
+  if (action.type == SET_USER_SCOPE && action.scope)
     userScope = action.scope
-  }
 
   return {
     ...state,
